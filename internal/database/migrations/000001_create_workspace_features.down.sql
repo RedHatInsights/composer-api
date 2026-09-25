54695e4cc616ddb2cefbafb797151e1bffb7ce5e
Aaditi Jain <aaditi2290@gmail.com>
2026-09-25 13:31:35 +0530

    Updated At Trigger

--- internal/database/migrations/000001_create_workspace_features.down.sql
+++ internal/database/migrations/000001_create_workspace_features.down.sql
@@ -1,1 +1,2 @@
+DROP FUNCTION IF EXISTS update_updated_at() cascade;
 DROP TABLE IF EXISTS workspace_features;
--- internal/database/migrations/000001_create_workspace_features.up.sql
+++ internal/database/migrations/000001_create_workspace_features.up.sql
@@ -12,3 +12,12 @@ CREATE TABLE workspace_features (
 );
 
 CREATE INDEX idx_workspace_features_workspace ON workspace_features(workspace_id);
+
+CREATE OR REPLACE FUNCTION update_updated_at()
+RETURNS TRIGGER AS $$
+BEGIN NEW.updated_at = NOW(); RETURN NEW; END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_workspace_features_updated_at
+BEFORE UPDATE ON workspace_features
+FOR EACH ROW EXECUTE FUNCTION update_updated_at();
